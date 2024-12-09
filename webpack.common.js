a0f0f13cdc76c86412108cd961f49171e71a7cac
Full Name <me@Box.DomainName>
2024-12-09 13:56:09 -0600

    Add entry point.

--- webpack.common.js
+++ webpack.common.js
@@ -4,12 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 
     entry: {
-        app: '.src/index.js',
+        app: './src/index.js',
     },
 
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Production',
+            template: './src/template.html',
         }),
     ],
 
